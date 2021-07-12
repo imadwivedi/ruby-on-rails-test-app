0fe185bc79a15efe50610f6f801af36045c73ac5
Aniket Dwivedi <imadwivedi@gmail.com>
2021-07-12 09:09:36 +0530

    Added new DB Schema and taking users params
    Signed-off-by: Aniket Dwivedi <imadwivedi@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #get '/articles', to:'articles#index'
-  root 'articles#index'
-  get '/about', to:'about#index'
+  root "articles#index"
+
+  #get "/articles", to: "articles#index"
+  #get "/articles/:id", to: "articles#show"
+  resources :articles
 end
